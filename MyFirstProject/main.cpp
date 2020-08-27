255e44539dd5f1557c53f2488c487ee1799d9f0c
vanderlee1 <69871267+vanderlee1@users.noreply.github.com>
2020-08-27 13:54:34 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,24 @@
+/*
+Rebecca van der Lee
+C++ fall 2020
+Due:8/31/2020
+1 and Lab 1 Introduction to C++ Programming
+This lab will serve as a demonstration of your ability to download a project from GitHub,
+compile and run the project, upload a project to GitHub, and submit a project to Google
+Classroom. This lab assumes you have downloaded and installed an IDE
+(integrated development environment) and have also installed GitHub Desktop. 
+If you have not met the assumptions for the lab, the steps you need to complete the lab 
+may vary from what is here. The goals are for you to download a project, compile and run 
+that project, and to upload your own project. If you meet the goals, you will get the points
+for this lab. Follow the directions in this lab and complete the tasks. 
+If you have questions, please ask.
+*/
+
+#include<iostream>
+
+int main()
+{
+	std::cout << "Hello World" ;
+	std::cout << " Blue";
+}
+
